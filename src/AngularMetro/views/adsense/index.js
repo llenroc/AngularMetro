ba0bb2cf17eb4383bf2b97c0c2a9d51aaa70105e
zhaohejing <454258314@qq.com>
2017-04-07 09:13:46 +0800

    ww

--- src/AngularMetro/views/adsense/index.js
+++ src/AngularMetro/views/adsense/index.js
@@ -1,23 +1,24 @@
-﻿
-/* Setup blank page controller */
-angular.module('MetronicApp').controller('views.adsense.index', ['$scope','settings','$uibModal',
+﻿(function () {
+    angular.module('MetronicApp').controller('views.adsense.index', ['$scope', 'settings', '$uibModal',
+        function ($scope, settings, $uibModal) {
+            // ajax初始化
+            $scope.$on('$viewContentLoaded', function () {
+                App.initAjax();
+            });
+            var vm = this;
+            vm.add = function () {
+                var modal = $uibModal.open({
+                    templateUrl: '/views/adsense/modal.html',
+                    controller: 'views.adsense.modal as vm',
+                    backdrop: 'static',
+                    //resolve: {
+                    //    activity: function () { return activityid; },
+                    //}
+                });
+                modal.result.then(function (response) {
+                    //  vm.getactivitys();
+                })
+            }
+        }])
+})();
 
-    function ( $scope, settings, $uibModal) {
-    $scope.$on('$viewContentLoaded', function () {
-        App.initAjax();
-    });
-    var vm = this;
-    vm.add = function () {
-        var modal = $uibModal.open({
-            templateUrl: '/views/adsense/modal.html',
-            controller: 'views.adsense.modal as vm',
-            backdrop: 'static',
-            //resolve: {
-            //    activity: function () { return activityid; },
-            //}
-        });
-        modal.result.then(function (response) {
-          //  vm.getactivitys();
-        })
-    }
-}]);
