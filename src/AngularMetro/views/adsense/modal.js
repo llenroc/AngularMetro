076556d420a561b89480e93d0a5194803fb9600d
zhaohejing <454258314@qq.com>
2017-04-12 11:05:25 +0800

    www

--- src/AngularMetro/views/adsense/modal.js
+++ src/AngularMetro/views/adsense/modal.js
@@ -80,7 +80,7 @@
 
 
 
-            var uploader = Qiniu.uploader({
+            vm.up = Qiniu.uploader({
                 runtimes: 'html5,flash,html4',      // 上传模式，依次退化
                 browse_button: 'pickfiles',         // 上传选择的点选按钮，必需
                 // 在初始化时，uptoken，uptoken_url，uptoken_func三个参数中必须有一个被设置
@@ -97,28 +97,16 @@
                 // Ajax请求downToken的Url，私有空间时使用，JS-SDK将向该地址POST文件的key和domain，服务端返回的JSON必须包含url字段，url值为该文件的下载地址
                 // unique_names: true,              // 默认false，key为文件名。若开启该选项，JS-SDK会为每个文件自动生成key（文件名）
                 // save_key: true,                  // 默认false。若在服务端生成uptoken的上传策略中指定了sava_key，则开启，SDK在前端将不对key进行任何处理
-                domain: '<Your bucket domain>',     // bucket域名，下载资源时用到，必需
-                container: 'container',             // 上传区域DOM ID，默认是browser_button的父元素
+                domain: 'ohr1coqtq.bkt.clouddn.com',     // bucket域名，下载资源时用到，必需
+                container: 'con',             // 上传区域DOM ID，默认是browser_button的父元素
                 max_file_size: '100mb',             // 最大文件体积限制
                 flash_swf_url: 'Moxie.swf',  //引入flash，相对路径
                 max_retries: 3,                     // 上传失败最大重试次数
                 dragdrop: true,                     // 开启可拖曳上传
-                drop_element: 'container',          // 拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-                chunk_size: '4mb',                  // 分块上传时，每块的体积
+             //   drop_element: 'con',          // 拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+                chunk_size: '0mb',                  // 分块上传时，每块的体积
                 auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
-                //x_vars : {
-                //    查看自定义变量
-                //    'time' : function(up,file) {
-                //        var time = (new Date()).getTime();
-                // do something with 'time'
-                //        return time;
-                //    },
-                //    'size' : function(up,file) {
-                //        var size = file.size;
-                // do something with 'size'
-                //        return size;
-                //    }
-                //},
+
                 init: {
                     'FilesAdded': function (up, files) {
                         plupload.each(files, function (file) {
