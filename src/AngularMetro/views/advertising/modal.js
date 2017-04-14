62d965818b65e37ad0180159c58571184d7f77b2
zhaohejing <454258314@qq.com>
2017-04-14 10:52:22 +0800

    啊

--- src/AngularMetro/views/advertising/modal.js
+++ src/AngularMetro/views/advertising/modal.js
@@ -7,6 +7,27 @@
             });
             var vm = this;
             vm.model = model;
+            vm.date = {
+                leftopen: false,
+                rightopen: false,
+
+                inlineOptions: {
+                    showWeeks: false
+                },
+                dateOptions: {
+                    //dateDisabled: disabled,
+                    formatYear: 'yyyy',
+                    maxDate: new Date(5000, 1, 1),
+                    minDate: new Date(1900, 1, 1),
+                    startingDay: 1
+                },
+                openleft: function () {
+                    vm.date.leftopen = !vm.date.leftopen;
+                },
+                openright: function () {
+                    vm.date.rightopen = !vm.date.rightopen;
+                }
+            }
             vm.save = function () {
                 if (!vm.model.atonce) {
                     if (!vm.model.startTime || !vm.model.endTime) {
