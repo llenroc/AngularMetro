dd3a4d479cdeb63d3a2b5ce8c718e831534fc019
zhaohejing <454258314@qq.com>
2017-04-14 16:55:34 +0800

    aa

--- src/AngularMetro/main.js
+++ src/AngularMetro/main.js
@@ -160,7 +160,6 @@ MetronicApp.controller('FooterController', ['$scope', function ($scope) {
 MetronicApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     // Redirect any unmatched url
     $urlRouterProvider.otherwise("/adsense.html");
-
     $stateProvider
         //广告资源管理
         .state("adsense", {
