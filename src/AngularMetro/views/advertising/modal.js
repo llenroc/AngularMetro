1638293d24237c85804b75ef589e0dbd11bb64ad
zhaohejing <454258314@qq.com>
2017-05-12 11:42:04 +0800

    www

--- src/AngularMetro/views/advertising/modal.js
+++ src/AngularMetro/views/advertising/modal.js
@@ -1,6 +1,6 @@
 ﻿angular.module('MetronicApp').controller('views.advertising.modal',
-    ['$scope', 'settings', '$uibModalInstance', 'model', 'dataFactory',
-        function ($scope, settings, $uibModalInstance, model, dataFactory) {
+    ['$scope', 'settings', '$uibModalInstance', 'model', 'dataFactory','$filter',
+        function ($scope, settings, $uibModalInstance, model, dataFactory,$filter) {
             $scope.$on('$viewContentLoaded', function () {
                 App.initAjax();
 
@@ -30,11 +30,24 @@
             }
             vm.save = function () {
                 if (!vm.model.atonce) {
+                    vm.model.atonce = false;
                     if (!vm.model.startTime || !vm.model.endTime) {
                         abp.notify.warn("请选择分发时间段");
                         return;
+                    } else {
+                        vm.model.startTime = $filter('date')(vm.model.startTime, "yyyy-MM-dd");
+                        vm.model.endTime = $filter('date')(vm.model.endTime, "yyyy-MM-dd");
+                    }
+                } else if (vm.model.atonce) {
+                    if ( !vm.model.endTime) {
+                        abp.notify.warn("请选择截止时间");
+                        return;
+                    } else {
+                        vm.model.endTime = $filter('date')(vm.model.endTime, "yyyy-MM-dd");
                     }
                 }
+
+
                 dataFactory.action("api/distribution/add", "", null, vm.model).then(function (res) {
                     if (res.result == "1") {
                         abp.notify.success("分发成功");
