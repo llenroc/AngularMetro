237537a6ab38e3d8b150ecb2ff68b2e69be548d5
zhaohejing <454258314@qq.com>
2017-07-07 12:44:30 +0800

    a

--- src/AngularMetro/views/fashionable/index.js
+++ src/AngularMetro/views/fashionable/index.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     angular.module('MetronicApp').controller('views.fashionable.index',
-        ['$scope', 'settings', "$stateParams", '$state', 'dataFactory', '$uibModal',
-        function ($scope, settings, $stateParams, $state, dataFactory, $uibModal) {
+        ['$scope', 'settings', "$stateParams", '$state', 'dataFactory',
+        function ($scope, settings, $stateParams, $state, dataFactory) {
             // ajax初始化
             $scope.$on('$viewContentLoaded', function () {
                 App.initAjax();
